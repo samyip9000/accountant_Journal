d868f73999c52b4dc250a2a92e841ab83b292eab
samyip9000 <samyip9000@gmail.com>
2023-02-13 19:28:30 +0800

    Finish exercise

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+
 // Require Node Packages
 const express = require("express");
 const { create } = require("express-handlebars");
 // const basicAuth = require("express-basic-auth");
+const fileUpload = require("express-fileupload");
+const fs = require("fs");
+const path = require("path");
 
 // Set up express and environment
 const app = express();
@@ -31,6 +35,7 @@ app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
 // Setup middleware and serve public folder
+app.use(fileUpload());
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -42,6 +47,84 @@ app.use(express.json());
 //     challenge: true,
 //   })
 // );
+
+//file upload
+// instantiate variables
+const cache = {};
+const uploadDirectory = __dirname + path.sep + "uploaded";
+
+// Promise Version of functions -- seperated
+function write(name, data) {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(uploadDirectory + path.sep + name, data, (error) => {
+      if (error) {
+        console.log(error);
+        reject(error);
+      }
+      resolve(name);
+    });
+  }).then(read);
+}
+
+function read(file) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(uploadDirectory + path.sep + file, (err, data) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(data);
+    });
+  });
+}
+
+// handle a request to list out all of the files within the folder uploaded
+app.get("/directoryInformation", (req, res) => {
+  fs.readdir(__dirname + "/uploaded", (error, information) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(information);
+      res.send(information);
+    }
+  });
+});
+
+// handle file being sent to the server
+app.post("/", (req, res) => {
+  console.log(req.files.file);
+  if (req.files.file) {
+    cache[req.files.file.name] = write(
+      req.files.file.name,
+      req.files.file.data
+    );
+    cache[req.files.file.name].then(() => {
+      res.send(req.files.file.name);
+      // res.redirect("/"); // use the inbuilt html form methods post and action
+    });
+  }
+});
+
+// handle downloading a file from server
+app.get("/uploaded/:filename", (req, res) => {
+  console.log(cache, "<<<<< cache");
+  // handle download if the item is in cache
+  if (cache[req.params.filename]) {
+    console.log(" in cache?");
+    cache[req.params.filename].then((data) => {
+      res.send(data);
+    });
+    // handle the download by first storing value in cache then giving it back
+  } else {
+    console.log(" not in cache?");
+    cache[req.params.filename] = read(req.params.filename);
+    cache[req.params.filename].then((data) => {
+      console.log(cache, "<<<< cache after population");
+      res.send(data);
+    });
+  }
+});
+
+//index router
 
 app.get("/", (req, res) => {
   res.render("index", {
--- public/app.js
+++ public/app.js
@@ -1,168 +1,194 @@
+app.js;
 let accountData = [
-    {
-        id:202301001,
-        type:"debit",
-        date:20230101,
-        account:"sale",
-        amount:200,
-        myFileVal:"file.pdf" 
-    },
-        {
-        id:202301001,
-        type:"credit",
-        date:20230101,
-        account:"HSBC",
-        amount:200,
-        myFileVal:"file.pdf" 
-    },
-        {
-        id:202301002,
-        type:"debit",
-        date:20230105,
-        account:"promotion",
-        amount:1200,
-        myFileVal:"receipt.pdf" 
-    }
+  {
+    id: 202301001,
+    type: "debit",
+    date: 20230101,
+    account: "Sale",
+    amount: 200,
+    myFileVal: "202301001receipt.png",
+  },
+  {
+    id: 202301001,
+    type: "credit",
+    date: 20230101,
+    account: "HSBC",
+    amount: 200,
+    myFileVal: "202301001receipt.png",
+  },
+  {
+    id: 202301002,
+    type: "debit",
+    date: 20230102,
+    account: "Promotion",
+    amount: 1200,
+    myFileVal: "202301002receipt.png",
+  },
+  {
+    id: 202301002,
+    type: "debit",
+    date: 20230102,
+    account: "Heng Seng Bank",
+    amount: 1200,
+    myFileVal: "202301002receipt.png",
+  },
 ];
 
-let typeVal = "";
-let dateVal = "";
-let accountVal = "";
-let amountVal = 0;
-let myFileVal = "";
-let idSeed = 0;
-let accountID = 0;
-let idList = ["202301001","202301002"];
+let creditAccountList = ["Total", "HSBC", "Heng Seng Bank"];
 
+let idSeed = 1;
+let idList = ["202301001", "202301002"];
 
-function handleAccountTitle(id){
-    console.log(id)
-    if(id === "debit"){
-        //change title
-        document.querySelector(`label[for="account"]`).innerHTML = `Debit Account`;
-        //show new account
-        document.querySelector("#newAccountID").classList.remove("hidden");
-        document.querySelector("#account").innerHTML = accountID;
-        //hide exist account
-        document.querySelector("#existAccountID").classList.add("hidden");
-    } else if(id === "credit"){
-        //change title
-        document.querySelector(`label[for="account"]`).innerHTML = `Credit Account`;
-        //hide new account
-        document.querySelector("#newAccountID").classList.add("hidden");
-        //add exist ID option
-        let idOption = "";
-        for (let i = 0; i < idList.length; i++) {
-            idOption+= `<option value="${idList[i]}">`
-        }
-        document.querySelector("#existAccountID").classList.remove("hidden");
-        //show exist ID
-        document.querySelector(`datalist[id="existAccountID]`);
-    }
+// when date clicked
+function handleDate() {
+  let date = document.querySelector("#debitDate").value;
+  console.log(date);
+  document.querySelector("#creditDate").value = date;
+  handleJournalID(date);
 }
 
-function submitVal(e){
-    e.preventdefault();
+function handleJournalID(date) {
+  //generate journalID
+  let idYear = date.slice(0, 4);
+  let idMonth = date.slice(5, 7);
+  let fullID = "";
+  if (idSeed < 10) {
+    fullID = "00" + idSeed.toString();
+  } else if (idSeed < 100) {
+    fullID = "0" + idSeed.toString();
+  } else {
+    fullID = idSeed;
+  }
+  let journalID = idYear + idMonth + fullID;
+  document.querySelector("#debitJournalId").value = journalID;
+  document.querySelector("#creditJournalId").value = journalID;
 
-    if (document.querySelector('input[name="type"]').checked === false){
-        return;
-    }
-    //save form data
-    typeVal = document.querySelector('input[name="type"]:checked').id 
-    dateVal = document.querySelector("#date").value;
-    accountVal = document.querySelector("#account").value;
-    amountVal = document.querySelector("#amount").value;
-    myFileVal = document.querySelector("#myFile").value;
+  console.log(journalID);
 
-    console.log(typeVal)
-    console.log(dateVal)
-    console.log(accountVal)
-    console.log(amountVal)
-    console.log(myFileVal)
-
-    //generate accountID
-    let idYear = dateVal.slice(0,4);
-    let idMonth = dateVal.slice(5,7);
-    idSeed++;
-    let fullID = "";
-    if(idSeed<10){
-        fullID = "00"+idSeed.toString();
-    }else if(idSeed<100){
-        fullID = "0"+idSeed.toString();
-    }else{
-        fullID=idSeed;
-    }
-    let accountID = idYear + idMonth + fullID;
-    idList.push(accountID);
-    let idList = [];
-    console.log(accountID);
-    //create object
-    inputData = {
-        id:accountID,
-        type:typeVal,
-        date:dateVal,
-        account:accountVal,
-        amount:amountVal,
-        myFileVal:myFileVal
-    }
-    accountData.push(inputData);
-    console.log(accountData);
+  idList.push(journalID);
+  console.idList;
+  idSeed++;
 }
 
+function handleAmount() {
+  let amount = document.querySelector("#debitAmount").value;
+  console.log(amount);
+  document.querySelector("#creditAmount").value = amount;
+}
 
-// function init(){
-//     getAllData();
-// };
+function submitVal(e) {
+  e.preventdefault();
+  //if the input is not fill, it return
+  if (
+    document.querySelector("#date").value === "" ||
+    document.querySelector("#debitAccount").value === "" ||
+    document.querySelector("#debitAmount").value === "" ||
+    document.querySelector("#myFile").value === "" ||
+    document.querySelector("#creditAccount").value === "" ||
+    document.querySelector("#creditAmount").value === ""
+  ) {
+    return;
+  }
+  //save debit form data
+  debitData = {
+    id: document.querySelector("#debitJournalId").value,
+    type: "debit",
+    date: document.querySelector("#date").value,
+    account: document.querySelector("#debitAccount").value,
+    amount: document.querySelector("#debitAmount").value,
+    myFile: document.querySelector("#myFile").value,
+  };
+  //save credit form data
+  creditData = {
+    id: document.querySelector("#creditJournalId").value,
+    type: "credit",
+    date: document.querySelector("#date").value,
+    account: document.querySelector("#creditAccount").value,
+    amount: document.querySelector("#creditAmount").value,
+    myFile: document.querySelector("#myFile").value,
+  };
 
-// function getAllData(){
-//     fetch("/data").then(function(response){
-//         return response.json();
-//     }).then(function(responseData){
-//         console.log(responseData);
-//         stockData = responseData;
-//     })
-// }
+  //create object
+  accountData.push(debitData);
+  accountData.push(creditData);
+  console.log(accountData);
 
-function renderTable(){
-    for (let i = 0; i < accountData.length; i++) {
-        //debit account make new item
-        let resultHTML = "";
-        if(accountData[i].type ==="debit"){
-            resultHTML+=`
-            <div class="sheet-item" id="${accountData[i].id}">
-                <div class="sheet-item-left">
-                    <ul class="debit-item">
-                        <li>${accountData[i].type}</li>
-                        <li>${accountData[i].id}</li>
-                        <li>${accountData[i].date}</li>
-                        <li>${accountData[i].account}</li>
-                        <li>${accountData[i].amount}</li>
-                    </ul>
-                    <ul class="credit-item" id="credit${accountData[i].id}">
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                    </ul>
+  //handle upload file
+  var formData = new FormData();
+  formData.append("file", $("#fileInput")[0].files[0]);
+  console.log(formData);
+
+  $.ajax({
+    url: "/",
+    type: "POST",
+    data: formData,
+    processData: false,
+    contentType: false,
+    success: function (response) {
+      console.log(response);
+      fileList.append(
+        `<a class='file' href="/uploaded/${response}">${response}</a><br /><button class="delete" data-name="${response}">Delete ${response}</button><br />`
+      );
+    },
+  });
+
+  renderTable();
+}
+
+function init() {
+  renderTable();
+  counterProfitAndLoss();
+}
+
+function renderTable() {
+  for (let i = 0; i < accountData.length - 1; i++) {
+    //debit account make new item
+    let resultHTML = "";
+    if (i === 0 || i % 2 === 0) {
+      resultHTML += `
+                <div class="sheet-item">
+                    <div class="sheet-item-left">
+                        <ul class="debit-item">
+                            <li>${accountData[i].id}</li>
+                            <li>Debit</li>
+                            <li>${accountData[i].date}</li>
+                            <li>${accountData[i].account}</li>
+                            <li>${accountData[i].amount}</li>
+                        </ul>
+                        <ul class="credit-item">
+                            <li>${accountData[i + 1].id}</li>
+                            <li>Credit</li>
+                            <li>${accountData[i + 1].date}</li>
+                            <li>${accountData[i + 1].account}</li>
+                            <li>${accountData[i + 1].amount}</li>
+                        </ul>
+                    </div>
+                    <div class="sheet-item-right">
+                        <a class='file' href="/uploaded/${
+                          accountData[i].myFileVal
+                        }">${accountData[i].myFileVal}</a>
+                    </div>
                 </div>
-                <div class="sheet-item-right">
-                    ${accountData[i].myFileVal}
-                </div>
-            </div>
             `;
-            document.querySelector("#itemContainer").innerHTML+=resultHTML;
-        }
-        if(accountData[i].type ==="credit"){
-            resultHTML+=`
-                <li>${accountData[i].type}</li>
-                <li>${accountData[i].id}</li>
-                <li>${accountData[i].date}</li>
-                <li>${accountData[i].account}</li>
-                <li>${accountData[i].amount}</li>
-            `;
-            // document.getElementById(`credit${accountData[i].id}]`).innerHTML+=resultHTML;
-        }
     }
+    document.querySelector("#itemContainer").innerHTML += resultHTML;
+  }
 }
-// init();
+
+function counterProfitAndLoss() {
+  //total P&L
+  let totalProfitAndLoss = 0;
+  for (let i = 0; i < accountData.length; i++) {
+    if (i === 0 || i % 2 === 0) {
+      totalProfitAndLoss += accountData[i].amount;
+    }
+  }
+  document.querySelector("#totalProfitAndLoss").innerHTML = totalProfitAndLoss;
+
+  //Credit Account P&L
+  for (let i = 0; i < creditAccountList.length; i++) {
+    let creditAccount = creditAccountList[i];
+  }
+}
+
+init();
